7152bd4a834158e654735afa4d74838061ad858e
marshall229 <36282437+marshall229@users.noreply.github.com>
2018-05-04 10:44:49 -0700

    Update graph.h

--- graph.h
+++ graph.h
@@ -11,29 +11,36 @@
 
 #pragma once
 
+#include <iostream>
 #include <string>
+#include <fstream>
+#include <vector>
+
+
 using namespace std;
 
 class Graph {
- public:
-  Graph(); // default constructor
-  Graph(int rno, float rbudget); // constructor with two arguments representing the number of nodes, initial budget
-  void addEdge(int node1, int node2); // adds an edge between two nodes in the graph node1 and node2
-  void setValue(int node, float rval); // sets a value for a node
-  void setBudget(float rbu); // sets the initial budget
-  int getNSize(); // return number of nodes
-  int getESize(); // return number of edges
-  float getBudget(); // return current budget
-  float getValue(int node) ; // returns the value of the node
-  void readData (string fileName); // reads data from a specified file
-  int DFS(int startNode); //return the number of nodes visited using BFS starting at startNode and accumulating values at each node, as long as the budget remains positive
+public:
+	Graph(); // default constructor
+	Graph(int rno, float rbudget); // constructor with two arguments representing the number of nodes, initial budget
+	void addEdge(int node1, int node2); // adds an edge between two nodes in the graph node1 and node2
+	void setValue(int node, float rval); // sets a value for a node
+	void setBudget(float rbu); // sets the initial budget
+	int getNSize(); // return number of nodes
+	int getESize(); // return number of edges
+	float getBudget(); // return current budget
+	float getValue(int node); // returns the value of the node
+	void readData(string fileName); // reads data from a specified file
+	int DFS(int startNode); //return the number of nodes visited using BFS starting at startNode and accumulating values at each node, as long as the budget remains positive
 
-  // return the starting node that gives a longest DFS run before running out of budget
-  // if there are multiple nodes with the same DFS run length, return the smallest node
-  int bestStartVertex();
+							// return the starting node that gives a longest DFS run before running out of budget
+							// if there are multiple nodes with the same DFS run length, return the smallest node
+	int bestStartVertex();
 
- private:
-  // member variables and helper functions (if needed)
+private:
+	// member variables and helper functions (if needed)
+	int number_of_nodes;
+	float initial_budget;
 };
 
 
