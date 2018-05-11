a19a885e0491cf636e18ec31ff135c40b58c8906
marshall229 <36282437+marshall229@users.noreply.github.com>
2018-05-10 18:40:15 -0700

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -19,25 +19,25 @@ Graph::Graph(int rno, float rbudget) // constructor with two arguments represent
 {
 	number_of_nodes = rno;
 	budget = rbudget;
-    
-   //Sets adjacency matrix values equal to false
-    for(int i=0;i<number_of_nodes;i++){
-        for(int j=0;j<number_of_nodes;j++)
-        adjMat[i][j]=0;
-        }
-    
-    }
+
+	//Sets adjacency matrix values equal to false
+	for (int i = 0; i< rno; i++) {
+		for (int j = 0; j< rno; j++)
+			adjMat[i][j] = 0;
+	}
+
+}
 
 void Graph::addEdge(int node1, int node2) // adds an edge between two nodes in the graph node1 and node2
 {
-    adjMat[node1][node2]=1;
-    adjMat[node2][node1]=1;
-    counter++;
+	adjMat[node1][node2] = 1;
+	adjMat[node2][node1] = 1;
+	counter++;
 }
 void Graph::setValue(int node, float rval) // sets a value for
 {
-    //Not an issue with pair, its an issue with the vector
-    graph.insert(graph.begin()+node,make_pair(false,rval));
+	//Not an issue with pair, its an issue with the vector
+	graph.insert(graph.begin() + node, make_pair(false, rval));
 }
 void Graph::setBudget(float rbu) // sets the initial budget
 {
@@ -45,19 +45,22 @@ void Graph::setBudget(float rbu) // sets the initial budget
 }
 int Graph::getNSize() // return number of nodes
 {
-	return number_of_nodes;
+	if (graph.size() == 0)
+		return number_of_nodes;
+	else
+		return graph.size();
 }
 int Graph::getESize() // return number of edges
 {
-    return counter;
+	return counter;
 }
 float Graph::getBudget() // return current budget
 {
-    return budget;
+	return budget;
 }
 float Graph::getValue(int node) // returns the value of the node
 {
-    return graph[node].second;
+	return graph[node].second;
 }
 void Graph::readData(string fileName)// reads data from a specified file
 {
@@ -73,11 +76,11 @@ void Graph::readData(string fileName)// reads data from a specified file
 			myfile >> budget;
 			setBudget(budget);
 			Graph s(n, budget);
-			
+
 			for (int i = 0; i < n; i++)
 			{
 				myfile >> value;
-				setValue(i,value);
+				setValue(i, value);
 
 			}
 			while (myfile >> edge1 >> edge2)
@@ -87,34 +90,34 @@ void Graph::readData(string fileName)// reads data from a specified file
 		}
 		myfile.close();
 	}
-} 
+}
 int Graph::DFS(int startNode) //return the number of nodes visited using DFS starting at startNode and accumulating values at each node, as long as the budget remains positive
 {
-    list<int> visited;
-    int num_visited=0;
-    for(int i=0;i<number_of_nodes;i++){
-        //Pushes value of adjecent vertex onto visited stack
-        if ((adjMat[startNode][i]==1) && (graph[i].first==false)){
-            //Iterate through visited stack to see if node has already been visited, if it hasn't push it on
-            visited.push_back(i);
-            graph[i].first=true;
-            break;
-            }
-            //Subtracts the budget by node's float value
-            budget = budget+graph[i].second;
-            }
-    //maybe loop until stack is empty
-    while( (budget>=0) || (visited.size()==number_of_nodes)){
-         num_visited++;
-    DFS(visited.front());
-        
-    }
-    return num_visited;
+	list<int> visited;
+	int num_visited = 0;
+	for (int i = 0; i< graph.size(); i++) {
+		//Pushes value of adjecent vertex onto visited stack
+		if ((adjMat[startNode][i] == 1) && (graph[i].first == false)) {
+			//Iterate through visited stack to see if node has already been visited, if it hasn't push it on
+			visited.push_back(i);
+			graph[i].first = true;
+			break;
+		}
+		//Subtracts the budget by node's float value
+		budget = budget + graph[i].second;
+	}
+	//maybe loop until stack is empty
+	while ((budget >= 0) || (visited.size() == graph.size())) {
+		num_visited++;
+		DFS(visited.front());
+
+	}
+	return num_visited;
 }
 
 // return the starting node that gives a longest DFS run before running out of budget
 // if there are multiple nodes with the same DFS run length, return the smallest node
 int Graph::bestStartVertex()
 {
-    return 0;
+	return 0;
 }
